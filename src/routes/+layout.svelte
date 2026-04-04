3fba1ab962075d4c83c8381ecda9878e0b2f554c
Theo <theobbb64@gmail.com>
2026-04-04 14:52:54 -0400

    first

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,12 @@
 <script lang="ts">
 	import '$lib/style/layout.css';
 	import '$lib/style/fonts.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import Footer from './footer.svelte';
-	import {
-		init_store_student_projects,
-		use_store_student_projects
-	} from '$lib/store/store-preview-student.svelte';
-	import { onNavigate } from '$app/navigation';
-	import { page } from '$app/state';
-	import { init_seed } from '$lib/store/seed-ctx.svelte';
 
 	const { data, children } = $props();
-
-	const archive_page = $derived(page.route.id == '/archives');
-	// init_store_student_projects();
-	// const store_student_projects = use_store_student_projects();
-
-	// onNavigate(() => {
-	// 	store_student_projects.current = null;
-	// });
-
-	init_seed(data.seed);
 </script>
 
 <div class="mx-4 grid min-h-svh grid-rows-[1fr_auto] pt-4 font-[350] lg:mx-gap lg:text-lg">
 	<div>
 		{@render children()}
 	</div>
-	<Footer />
 </div>
-
-<!-- <svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head> -->
--- src/routes/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export async function load({ parent }) {
-	const { years } = await parent();
-
-	redirect(307, `/${years[0]?.id}`);
-}
