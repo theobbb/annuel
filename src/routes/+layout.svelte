33cbbd51be6a36a862df73ad6e8ffa3c930f6eb8
Theo <theobbb64@gmail.com>
2026-04-04 14:58:58 -0400

    d

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,21 @@
 	import '$lib/style/fonts.css';
 
 	const { data, children } = $props();
+
+	const favicon_svg = $derived(
+		`
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+            <rect width="32" height="32" fill="yellow" />
+        </svg>
+    `.trim()
+	);
+
+	const favicon_data_uri = $derived(`data:image/svg+xml;base64,${btoa(favicon_svg)}`);
 </script>
 
-<div class="mx-4 grid min-h-svh grid-rows-[1fr_auto] pt-4 font-[350] lg:mx-gap lg:text-lg">
-	<div>
-		{@render children()}
-	</div>
-</div>
+<svelte:head>
+	<link rel="icon" href={favicon_data_uri} />
+	<title>Annuel de design 2026</title>
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="flex min-h-svh flex-col items-center justify-center">
+	<div class="">Annuel de design 2026 — Site web en construction...</div>
+	<a class="mt-1" href="https://www.instagram.com/annuel.design/" target="_blank"
+		>instagram.com/annuel.design</a
+	>
+</div>
